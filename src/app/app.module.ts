295f8fead0881d9121a85dcf079977280aa524ec
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-10-30 17:33:33 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {ProductService} from '../app/product.service'
+
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-
+import { ProductService } from '../app/product.service'
 @NgModule({
   declarations: [
     AppComponent,
