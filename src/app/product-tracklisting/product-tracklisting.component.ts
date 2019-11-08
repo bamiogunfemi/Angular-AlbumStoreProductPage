4eefdbcd5585e1fd2032522f4dc37e8cae1bfc02
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-08 15:36:28 +0100

    Modle Complete

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-tracklisting.',
+  templateUrl: './product-tracklisting..component.html',
+  styleUrls: ['./product-tracklisting..component.css']
+})
+export class ProductTracklisting.Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
