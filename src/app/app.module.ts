9ce4b9703ef30ec1b4800c15d942ce98eda600a7
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-08 18:39:44 +0100

    Module Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
-import { ProductTracklisting, ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+
+const appRoute: Routes =[
+  {path: 'products', component:ProductListComponent},
+  {path: 'product/:id', component:ProductPageComponent },
+  {path: '', redirectTo:'products', pathMatch: 'full'}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTracklisting.Component,
     ProductTracklistingComponent,
     ProductListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+     RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
