27355a132284807706161146c95985002a219fe2
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-08 16:07:40 +0100

    Module Complete

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Album} from '../album'
-import { ProductService} from '../product.service'
+import {ProductService } from '../product.service'
 @Component({
   selector: 'app-product-tracklisting',
   templateUrl: './product-tracklisting.component.html',
@@ -8,11 +8,11 @@ import { ProductService} from '../product.service'
 })
 export class ProductTracklistingComponent implements OnInit {
   albumInfo: Album;
-  constructor(private _productService:ProductService) { }
+  constructor( private _productService:ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response=>
-      this.albumInfo = response);
-  }  
+      this.albumInfo = response)
+  }
 
 }
