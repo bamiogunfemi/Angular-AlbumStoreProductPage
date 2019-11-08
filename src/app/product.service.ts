18f45b12a0485814209b3778246e7cd8b0e22841
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-08 14:37:20 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,9 +8,9 @@ private _albumUrl ='../assets/album.json';
 
   constructor(private _http: Http) { }
 
-    getAlbum( id : number){
-      return this._http.get(this._albumUrl).map((response)=>{
-        response.json()
-      })
+   getAlbum( id : number){
+      return this._http.get(this._albumUrl).map((response)=>
+        response.json())
+      }
     }
-}
+
